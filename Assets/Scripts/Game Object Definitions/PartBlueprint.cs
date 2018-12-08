5694802c47b7372302a6c0ffdec66cecd42a0ca3
Ormanus <osalminenster@gmail.com>
2018-12-09 01:33:01 +0200

    ResourceManager

---
+++ Assets/Scripts/Game Object Definitions/PartBlueprint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Part", menuName = "ShellCore/Part", order = 2)]
+public class PartBlueprint : ScriptableObject
+{
+    public string spriteID;
+    public float health;
+    public float mass;
+    public Ability.AbilityType type;
+    public bool requiresShooter;
+    public string shooterSpriteID;
+}
