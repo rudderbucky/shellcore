df5bfcc10efdd4ec37c7b4b358065e2b2619a828
Ormanus <osalminenster@gmail.com>
2018-12-16 17:15:07 +0200

    Tank pathfinding
    Still a bit buggy, but mostly works

--- Assets/Scripts/Game Object Definitions/Entity Definitions/Tank.cs
+++ Assets/Scripts/Game Object Definitions/Entity Definitions/Tank.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Tank : GroundCraft
 {
+    Vector2[] path;
+    int index = 0;
+    bool hasPath = false;
+
     protected override void Start()
     {
         base.Start();
@@ -26,15 +30,58 @@ public class Tank : GroundCraft
         }
     }
 
+    private void pathfindToTarget()
+    {
+        Entity[] entities = FindObjectsOfType<Entity>();
+        Entity target = null;
+        float minD = float.MaxValue;
+        for(int i = 0; i < entities.Length; i++)
+        {
+            if (entities[i].faction == faction)
+                continue;
+            float d2 = (transform.position - entities[i].transform.position).sqrMagnitude;
+            if(d2 < minD)
+            {
+                minD = d2;
+                target = entities[i];
+            }
+        }
+
+        path = LandPlatformGenerator.pathfind(transform.position, target.transform.position);
+        hasPath = (path != null);
+
+        if (path != null)
+            index = path.Length - 1;
+    }
+
     protected virtual void drive()
     {
         isImmobile = !isOnGround;
 
-        Vector3 direction = LandPlatformGenerator.getDirection(transform.position);
-
-        if (direction != Vector3.zero && LandPlatformGenerator.CheckOnGround(transform.position + direction * 0.5f))
+        if (isImmobile)
         {
-            MoveCraft(direction);
+            hasPath = false;
+        }
+        else
+        {
+            if (hasPath)
+            {
+                Vector2 direction = path[index] - (Vector2)transform.position;
+                MoveCraft(direction.normalized);
+
+                if (direction.magnitude < 0.1f)
+                {
+                    index--;
+                    if (index < 0)
+                    {
+                        hasPath = false;
+                    }
+                }
+            }
+            else
+            {
+                pathfindToTarget();
+            }
         }
     }
 }
--- Assets/Scripts/SFX Scripts/LandPlatformGenerator.cs
+++ Assets/Scripts/SFX Scripts/LandPlatformGenerator.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class LandPlatformGenerator : MonoBehaviour {
 
     public GameObject[] prefabs;
-    public LandPlatform blueprint;
+    //public LandPlatform blueprint;
+    [HideInInspector]
+    public int rows = 1, columns = 1;
+
+    [HideInInspector]
+    public int[] tilemap = new int[1];
+
     private static List<GameObject> tiles;
-    private static List<int> directions;
-    // Use this for initialization
+    private static List<Rect> areas;
+    private static List<NavigationNode> nodes;
+
+    private static float tileSize;
 
     public static bool CheckOnGround(Vector3 position)
     {
-        for(int i = 0; i < tiles.Count; i++)
+        for (int i = 0; i < tiles.Count; i++)
         {
-            if(tiles[i].GetComponent<SpriteRenderer>().bounds.Contains(position))
+            if (tiles[i].GetComponent<SpriteRenderer>().bounds.Contains(position))
             {
                 return true;
             }
@@ -24,78 +35,372 @@ public class LandPlatformGenerator : MonoBehaviour {
 
     public static Vector3 getDirection(Vector3 position)
     {
-        int tileIndex = -1;
-        for (int i = 0; i < tiles.Count; i++)
-        {
-            if (tiles[i].GetComponent<SpriteRenderer>().bounds.Contains(position))
-            {
-                tileIndex = i;
-                break;
-            }
-        }
-        if (tileIndex == -1)
-            return Vector3.zero;
-
-        Vector3 center = tiles[tileIndex].transform.position;
-        float e = 0.1f;
-        Vector3 direction = Vector3.zero;
-
-        switch (directions[tileIndex])
-        {
-            case 0:
-                if (position.y < center.y - e)
-                    direction += Vector3.up;
-                else if (position.y > center.y + e)
-                    direction -= Vector3.up;
-                direction += Vector3.right;
-                break;
-            case 1:
-                if (position.x < center.x - e)
-                    direction += Vector3.right;
-                else if (position.x > center.x + e)
-                    direction -= Vector3.right;
-                direction += Vector3.up;
-                break;
-            case 2:
-                if (position.y < center.y - e)
-                    direction += Vector3.up;
-                else if (position.y > center.y + e)
-                    direction -= Vector3.up;
-                direction += -Vector3.right;
-                break;
-            case 3:
-                if (position.x < center.x - e)
-                    direction += Vector3.right;
-                else if (position.x > center.x + e)
-                    direction -= Vector3.right;
-                direction += -Vector3.up;
-                break;
-        }
-        return direction.normalized;
+        return Vector3.zero;
     }
 
-	void Start () {
-        tiles = new List<GameObject>();
-        directions = new List<int>();
-        if (blueprint)
+    struct NavigationNode
+    {
+        public static bool operator ==(NavigationNode x, NavigationNode y)
         {
-            float spacing = prefabs[1].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-            float ySpacing = 0;
-            for (int i = 0; i < blueprint.platformRows.Length; i++)
+            return x.pos == y.pos;
+        }
+        public static bool operator !=(NavigationNode x, NavigationNode y)
+        {
+            return x.pos != y.pos;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is NavigationNode))
             {
-                LandPlatform.Platform[] row = blueprint.platformRows[i].platformRow;
-                for (int j = 0; j < row.Length; j++)
+                return false;
+            }
+
+            var node = (NavigationNode)obj;
+            return pos.Equals(node.pos);
+        }
+
+        public override int GetHashCode()
+        {
+            return 991532785 + EqualityComparer<Vector2>.Default.GetHashCode(pos);
+        }
+
+        public Vector2 pos;
+        public List<NavigationNode> neighbours;
+        public List<float> distances;
+
+        public NavigationNode(Vector2 position)
+        {
+            pos = position;
+            neighbours = new List<NavigationNode>();
+            distances = new List<float>();
+        }
+    }
+
+    void Start() {
+        tiles = new List<GameObject>();
+        areas = new List<Rect>();
+
+        if (prefabs.Length > 0)
+        {
+            tileSize = prefabs[1].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
                 {
-                    if (row[j].type != 0)
+                    if (tilemap[i + j * rows] > -1 && tilemap[i + j * rows] < prefabs.Length && prefabs[tilemap[i + j * rows]] != null)
                     {
-                        GameObject tile = Instantiate(prefabs[row[j].type], new Vector3(j * spacing, ySpacing, 0), Quaternion.identity);
+                        GameObject tile = Instantiate(prefabs[tilemap[i + j * rows]], new Vector3(j * tileSize, i * tileSize, 0), Quaternion.identity);
                         tiles.Add(tile);
                         tile.transform.SetParent(transform);
-                        directions.Add(row[j].direction);
+
+                        areas.Add(new Rect(j * tileSize, i * tileSize, tileSize, tileSize));
                     }
                 }
-                ySpacing += spacing;
             }
         }
-	}
+
+        buildNodes();
+    }
+
+    void buildNodes()
+    {
+        Debug.Log("Building nodes...");
+
+        float dToCenter = tileSize / 3f; // node distance to center on one axis
+        nodes = new List<NavigationNode>();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (tilemap[i + j * rows] > -1)
+                {
+                    //create nodes
+                    if (!isValidTile(i, j))
+                        continue;
+
+                    bool right = isValidTile(i + 1, j);
+                    bool up = isValidTile(i, j + 1);
+                    bool left = isValidTile(i - 1, j);
+                    bool down = isValidTile(i, j - 1);
+
+                    if ((!right && !up) || (right && up && !isValidTile(i + 1, j + 1))) //check if the tile is a corner
+                        nodes.Add(new NavigationNode(new Vector2(j * tileSize + dToCenter, i * tileSize + dToCenter)));
+                    if ((!left && !up) || (left && up && !isValidTile(i - 1, j + 1)))
+                        nodes.Add(new NavigationNode(new Vector2(j * tileSize + dToCenter, i * tileSize - dToCenter)));
+                    if ((!left && !down) || (left && down && !isValidTile(i - 1, j - 1)))
+                        nodes.Add(new NavigationNode(new Vector2(j * tileSize - dToCenter, i * tileSize - dToCenter)));
+                    if ((!right && !down) || (right && down && !isValidTile(i + 1, j - 1)))
+                        nodes.Add(new NavigationNode(new Vector2(j * tileSize - dToCenter, i * tileSize + dToCenter)));
+                }
+            }
+        }
+
+        int debugCount = 0;
+
+        //connect nodes
+        Debug.Log("Connecting nodes...");
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            for (int j = i + 1; j < nodes.Count; j++)
+            {
+                if (isInLoS(nodes[i].pos, nodes[j].pos))
+                {
+                    nodes[i].neighbours.Add(nodes[j]);
+                    nodes[j].neighbours.Add(nodes[i]);
+                    float d = (nodes[i].pos - nodes[j].pos).magnitude;
+                    nodes[i].distances.Add(d);
+                    nodes[j].distances.Add(d);
+                    debugCount++;
+                }
+            }
+        }
+
+        Debug.Log("Done! Nodes: " + nodes.Count + " Connections: " + debugCount);
+    }
+
+    bool isValidTile(int x, int y)
+    {
+        return x < rows && y < columns && x >= 0 && y >= 0 && tilemap[x + y * rows] > -1 && tilemap[x + y * rows] < prefabs.Length && prefabs[tilemap[x + y * rows]] != null;
+    }
+
+    bool isInLoS(Vector2 p1, Vector2 p2)
+    {
+        Vector2 p12 = p1 / tileSize + new Vector2(0.5f, 0.5f);
+        Vector2 p22 = p2 / tileSize + new Vector2(0.5f, 0.5f);
+
+        float d = (p22 - p12).magnitude;
+
+        Vector2 step = (p22 - p12) / (d * 10f);
+        Vector2 point = p12;
+        float stepLength = step.magnitude;
+        for (float i = 0; i < d; i += stepLength)
+        {
+            if (!isValidTile(Mathf.FloorToInt(point.y), Mathf.FloorToInt(point.x)))
+            {
+                return false;
+            }
+            else
+            {
+                //TODO: check if the point is too close to a wall
+            }
+            point += step;
+        }
+
+        return true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        Vector2 mPos = v3;
+
+        if (areas != null)
+        {
+            for (int i = 0; i < areas.Count; i++)
+            {
+                if (new Rect(-0.5f * tileSize, -0.5f * tileSize, columns * tileSize, rows * tileSize).Contains(mPos))
+                {
+                    int x = Mathf.FloorToInt(mPos.y / tileSize + 0.5f);
+                    int y = Mathf.FloorToInt(mPos.x / tileSize + 0.5f);
+                    if (isValidTile(x, y))
+                    {
+                        Gizmos.color = new Color(0, 100, 150);
+                        Gizmos.DrawCube(new Vector3(y * tileSize, x * tileSize, 0), new Vector3(tileSize, tileSize, tileSize));
+                    }
+                }
+            }
+        }
+
+        if (nodes != null)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Gizmos.color = new Color(0, 100, 255);
+                Gizmos.DrawSphere(nodes[i].pos, 0.2f);
+                Gizmos.color = new Color(200, 0, 0, 100);
+                for (int j = 0; j < nodes[i].neighbours.Count; j++)
+                {
+                    Gizmos.DrawLine(nodes[i].pos, nodes[i].neighbours[j].pos);
+                }
+            }
+        }
+    }
+
+    private class PathfindNode
+    {
+        public NavigationNode node;
+        public PathfindNode parent;
+        public float d;
+
+        public PathfindNode(NavigationNode node, PathfindNode parent, float distance)
+        {
+            this.node = node;
+            this.parent = parent;
+            d = distance;
+        }
+    }
+
+    public static Vector2[] pathfind(Vector2 startPos, Vector2 targetPos)
+    {
+        if (nodes == null)
+            return null;
+
+        //find node closest to start and end positions
+        NavigationNode start = getNearestNode(startPos);
+        NavigationNode end = getNearestNode(targetPos);
+
+        if (start == end)
+            return null;
+
+        var openList = new List<PathfindNode>();
+        var closedList = new List<PathfindNode>();
+
+        openList.Add(new PathfindNode(start, null, 0f));
+
+        while (openList.Count > 0)
+        {
+            // Get next node with shortest total distance
+            PathfindNode current = openList[0];
+            float shortest = float.MaxValue;
+            for (int i = 0; i < openList.Count; i++)
+            {
+                if(openList[i].d < shortest)
+                {
+                    shortest = openList[i].d;
+                    current = openList[i];
+                }
+            }
+
+            // Check if the goal has been reached
+            if(current.node == end)
+            {
+                var path = new List<Vector2>();
+                PathfindNode node = current;
+                do
+                {
+                    path.Add(node.node.pos);
+                    node = node.parent;
+                }
+                while (node.parent != null);
+
+                return path.ToArray();
+            }
+
+            for(int i = 0; i < current.node.neighbours.Count; i++)
+            {
+                bool closed = false;
+                for(int j = 0; j < closedList.Count; j++)
+                {
+                    if(closedList[j].node == current.node.neighbours[i])
+                    {
+                        closed = true;
+                        break;
+                    }
+                }
+                if(!closed)
+                {
+                    openList.Add(new PathfindNode(current.node.neighbours[i], current, current.d + current.node.distances[i]));
+                }
+            }
+
+            openList.Remove(current);
+            closedList.Add(current);
+        }
+        return null;
+    }
+
+    static NavigationNode getNearestNode(Vector2 pos, bool los = false)
+    {
+        //TODO: (?) Adjust coordinates
+        NavigationNode nearest = nodes[0];
+        float minD = float.MaxValue;
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if(los)
+            {
+                // TODO: static -> singleton & use los here
+                continue;
+            }
+            float d = (pos - nodes[i].pos).sqrMagnitude;
+            if (d < minD)
+            {
+                nearest = nodes[i];
+                minD = d;
+            }
+        }
+        return nearest;
+    }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(LandPlatformGenerator))]
+class LandPlatformEditor : Editor
+{
+    SerializedProperty tilemap;
+    SerializedProperty rows;
+    SerializedProperty columns;
+
+    Vector2 scrollPos;
+
+    private void OnEnable()
+    {
+        tilemap = serializedObject.FindProperty("tilemap");
+        rows = serializedObject.FindProperty("rows");
+        columns = serializedObject.FindProperty("columns");
+
+        if (tilemap.arraySize < rows.intValue * columns.intValue)
+        {
+            serializedObject.Update();
+            tilemap.arraySize = rows.intValue * columns.intValue;
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        DrawDefaultInspector();
+
+        int oldRows = rows.intValue;
+        int oldColumns = columns.intValue;
+
+        // Edit size
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.PrefixLabel("Rows: ");
+        rows.intValue = EditorGUILayout.IntField(rows.intValue);
+        EditorGUILayout.PrefixLabel("Columns: ");
+        columns.intValue = EditorGUILayout.IntField(columns.intValue);
+        if (rows.intValue <= 0)
+            rows.intValue = 1;
+        if (columns.intValue <= 0)
+            columns.intValue = 1;
+
+        EditorGUILayout.EndHorizontal();
+
+        if (rows.intValue != oldRows || columns.intValue != oldColumns)
+            tilemap.arraySize = rows.intValue * columns.intValue;
+
+        using (var scrollView = new EditorGUILayout.ScrollViewScope(scrollPos, GUILayout.Width(columns.intValue*20 + 16), GUILayout.Height(rows.intValue * 20 + 16)))
+        {
+            scrollPos = scrollView.scrollPosition;
+            for (int i = 0; i < rows.intValue; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                for (int j = 0; j < columns.intValue; j++)
+                {
+                    SerializedProperty type = tilemap.GetArrayElementAtIndex(i + rows.intValue * j);
+                    type.intValue = EditorGUILayout.IntField(type.intValue, GUILayout.Width(16), GUILayout.Height(16));
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+#endif
