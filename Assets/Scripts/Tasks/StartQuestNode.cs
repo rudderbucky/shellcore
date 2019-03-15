224b354080b057d51691d57c7d5e5e83edefcc05
Ormanus <osalminenster@gmail.com>
2019-03-15 04:35:59 +0200

    Better structure, more nodes

---
+++ Assets/Scripts/Tasks/StartQuestNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NodeEditorFramework.Standard
+{
+    [Node(false, "TaskSystem/Start")]
+    public class StartQuestNode : Node
+    {
+        //Node things
+        public const string ID = "StartNode";
+        public override string GetID { get { return ID; } }
+
+        public override string Title { get { return "Start"; } }
+
+        public override bool AllowRecursion { get { return true; } }
+        public override bool ContinueCalculation { get { return true; } }
+
+        [ConnectionKnob("Output Right", Direction.In, "Flow", NodeSide.Right)]
+        public ConnectionKnob outputRight;
+    }
+}
