3f428f312e97765e79b907d0706a75cbee3072ed
Bruno Cardoso <brunocardosodo@gmail.com>
2019-03-29 22:34:16 -0300

    Fixed death while interacting #2
    Fixed typo in code (mine, sorry)

--- Assets/Scripts/Game Object Definitions/Entity Definitions/PlayerCore.cs
+++ Assets/Scripts/Game Object Definitions/Entity Definitions/PlayerCore.cs
@@ -111,7 +111,7 @@ public class PlayerCore : ShellCore {
     
 	// Update is called once per frame
 	protected override void Update () {
-        if((isInteracting) || (!isDead)) isImmobile = true;
+        if((isInteracting) || (isDead)) isImmobile = true;
         else isImmobile = false;
         // call methods
         if(group.sortingOrder < maxAirLayer) // player must always be above other entities
