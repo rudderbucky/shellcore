d4f4fe58c80ac2871b98d2c55603cea13a89609d
rudderbucky <anandkwork7@gmail.com>
2019-08-20 09:15:47 -0700

    version update

--- Assets/Scripts/HUD Scripts/SaveMenuHandler.cs
+++ Assets/Scripts/HUD Scripts/SaveMenuHandler.cs
@@ -91,7 +91,7 @@ public class SaveMenuHandler : GUIWindowScripts {
 		}
 	}
 	public void AddSave() {
-		string currentVersion = "Prototype 3.0.1";
+		string currentVersion = "Prototype 4.0.0";
 		string name = inputField.text.Trim();
 		string path = Application.persistentDataPath + "\\Saves" + "\\" + name;
 		inputField.transform.parent.GetComponentInChildren<GUIWindowScripts>().ToggleActive();
