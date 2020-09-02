26b36603f7ec117ee4c902093cc7f4d793a0eb14
rudderbucky <anandkwork7@gmail.com>
2020-09-02 16:23:52 -0700

    Fix inconsistent rigidbody/transform pos ref

--- Assets/Scripts/Game Object Definitions/Entity Definitions/AirConstruct.cs
+++ Assets/Scripts/Game Object Definitions/Entity Definitions/AirConstruct.cs
@@ -25,7 +25,7 @@ public class AirConstruct : Construct {
     {
         if(entityBody.velocity == Vector2.zero) {
             timePassed = timePassed + Time.deltaTime; // add to time so sin oscillates (this will start at zero the moment this loop begins)
-            oscillatorVector = transform.position; // get the current aircraft position
+            oscillatorVector = entityBody.position; // get the current aircraft position
             oscillatorVector.y = Mathf.Min(oscillatorVector.y - 0.005F * Mathf.Sin(timePassed + entityBody.position.x), storedPos.y); // cool math stuff 
             oscillatorVector.y = Mathf.Max(oscillatorVector.y, storedPos.y - 1); // more cool math stuff
             entityBody.MovePosition(oscillatorVector); // set the aircraft position
