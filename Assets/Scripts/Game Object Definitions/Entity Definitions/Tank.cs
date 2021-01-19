6040c9165dc6ade8d8babd965373792b1a9e7dde
Ormanus <osalminenster@gmail.com>
2021-01-19 19:15:20 +0200

    Tanks not automatically selected when bought, avoid collisions

--- Assets/Scripts/Game Object Definitions/Entity Definitions/Tank.cs
+++ Assets/Scripts/Game Object Definitions/Entity Definitions/Tank.cs
@@ -9,6 +9,18 @@ public class Tank : GroundCraft, IOwnable
     bool hasPath = false;
     IOwner owner;
     Vector2? pathfindTarget = null;
+    float pathfindTimer = 0f;
+
+    WeaponAbility weapon;
+    WeaponAbility Weapon
+    {
+        get
+        {
+            if (weapon == null)
+                weapon = GetComponentInChildren<WeaponAbility>();
+            return weapon;
+        }
+    }
 
     protected override void OnDeath()
     {
@@ -36,12 +48,21 @@ public class Tank : GroundCraft, IOwnable
     {
         base.Update();
 
+        if (pathfindTimer >= 0f)
+        {
+            pathfindTimer -= Time.deltaTime;
+            if (pathfindTimer <= 0f)
+            {
+                pathfindToTarget();
+            }
+        }
+
         if (isOnGround && !isDead)
         {
             TargetManager.Enqueue(targeter);
-            if (!isDead && GetComponentInChildren<WeaponAbility>() && !draggable.dragging)
+            if (!isDead && Weapon && !draggable.dragging)
             {
-                GetComponentInChildren<WeaponAbility>().Tick(0);
+                Weapon.Tick(0);
             }
             drive();
         }
@@ -62,7 +83,7 @@ public class Tank : GroundCraft, IOwnable
             if (FactionManager.IsAllied(entities[i].faction, faction))
                 continue;
             float d2 = (transform.position - entities[i].transform.position).sqrMagnitude;
-            if(d2 < minD && GetComponentInChildren<WeaponAbility>().CheckCategoryCompatibility(entities[i]))
+            if(d2 < minD && Weapon.CheckCategoryCompatibility(entities[i]))
             {
                 minD = d2;
                 target = entities[i];
@@ -70,16 +91,16 @@ public class Tank : GroundCraft, IOwnable
         }
 
         // If a target is found, find a path to it
-        if (target != null/* && pathfindTarget != target.transform.position*/)
+        if (target != null)
         {
             pathfindTarget = target.transform.position;
 
             path = null;
             if (target && (target.transform.position - transform.position).sqrMagnitude > 16)
-                path = LandPlatformGenerator.pathfind(transform.position, target.transform.position);
-            hasPath = (path != null);
+                path = LandPlatformGenerator.pathfind(transform.position, target.transform.position, Weapon.GetRange());
+            hasPath = (path != null && path.Length > 0);
 
-            if (path != null)
+            if (hasPath)
                 index = path.Length - 1;
         }
     }
@@ -91,6 +112,26 @@ public class Tank : GroundCraft, IOwnable
         {
             Vector2 direction = path[index] - (Vector2)transform.position;
 
+            const float minDistance = 2.0f;
+
+            // TODO: optimize?
+
+            var normalized = direction.normalized;
+            for (int i = 0; i < AIData.entities.Count; i++)
+            {
+                Entity e = AIData.entities[i];
+                if (e is GroundCraft && e != this && e.GetInstanceID() > GetInstanceID())
+                {
+                    float d = (e.transform.position - (transform.position)).sqrMagnitude;
+                    if (d < minDistance && pathfindTimer <= 0f)
+                    {
+                        hasPath = false;
+                        pathfindTimer = 0.5f;
+                        return;
+                    }
+                }
+            }
+
             if (direction.magnitude < 0.5f)
             {
                 index--;
@@ -101,14 +142,14 @@ public class Tank : GroundCraft, IOwnable
             }
             else if (index > 0 || direction.magnitude > 2F) 
             {
-                MoveCraft(direction.normalized);
+                MoveCraft(normalized);
             }
             else
-                MoveCraft(direction.normalized * 0.5F);
+                MoveCraft(normalized * 0.5F);
         }
-        else
+        else if (pathfindTimer <= 0f)
         {
-            pathfindToTarget();
+            pathfindTimer = 0.5f;
         }
     }
 
