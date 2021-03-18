04990a832c572c3e87c7714b1d122940fcd64691
Ormanus <osalminenster@gmail.com>
2021-03-18 18:40:45 +0200

    Dialogue Condition Node update

--- Assets/Scripts/Graphs/DialogueConditionCheckNode.cs
+++ Assets/Scripts/Graphs/DialogueConditionCheckNode.cs
@@ -41,13 +41,21 @@ namespace NodeEditorFramework.Standard
             "LesserThan"
         };
 
+        readonly string[] missionStatus = new string[]
+{
+            "Inactive",
+            "Ongoing",
+            "Complete"
+};
+
         readonly string[] variableTypes = new string[]
         {
             "Checkpoint",
             "Task Variable",
             "Reputation",
             "Parts Seen",
-            "Parts Obtained"
+            "Parts Obtained",
+            "Mission status"
         };
 
         public override void NodeGUI()
@@ -82,7 +90,14 @@ namespace NodeEditorFramework.Standard
                 variableName = GUILayout.TextArea(variableName);
                 GUILayout.EndHorizontal();
             }
-            
+            else if (variableType == 5)
+            {
+                GUILayout.Label("Mission Name:");
+                GUILayout.BeginHorizontal();
+                variableName = GUILayout.TextArea(variableName);
+                GUILayout.EndHorizontal();
+            }
+
 
             if (variableName.Equals(checkpointName, System.StringComparison.CurrentCulture))
                 checkpointName = "";
@@ -94,18 +109,23 @@ namespace NodeEditorFramework.Standard
 
             if (variableType > 0)
             {
-                GUILayout.Label("Value:");
-                value = RTEditorGUI.IntField(value);
+                if (variableType != 5)
+                {
+                    GUILayout.Label("Value:");
+                    value = RTEditorGUI.IntField(value);
+                }
 
                 GUILayout.Label("Comparison mode:");
                 //comparisonMode = GUILayout.SelectionGrid(comparisonMode, comparisonModes, 1, GUILayout.Width(128f));
-                if (GUILayout.Button(comparisonModes[comparisonMode]))
+                string[] comparisonTexts = variableType == 5 ? missionStatus : comparisonModes;
+
+                if (GUILayout.Button(comparisonTexts[comparisonMode]))
                 {
                     comparisonPopup = new PopupMenu();
                     comparisonPopup.SetupGUI();
-                    for (int i = 0; i < comparisonModes.Length; i++)
+                    for (int i = 0; i < comparisonTexts.Length; i++)
                     {
-                        comparisonPopup.AddItem(new GUIContent(comparisonModes[i]), false, SelectMode, i);
+                        comparisonPopup.AddItem(new GUIContent(comparisonTexts[i]), false, SelectMode, i);
                     }
                     comparisonPopup.Show(GUIScaleUtility.GUIToScreenSpace(GUILayoutUtility.GetLastRect().max));
                 }
@@ -138,6 +158,25 @@ namespace NodeEditorFramework.Standard
                 }
                 else
                     return 1;
+            }
+            else if (variableType == 5)
+            {
+                for (int i = 0; i < PlayerCore.Instance.cursave.missions.Count; i++)
+                {
+                    if (PlayerCore.Instance.cursave.missions[i].name == variableName)
+                    {
+                        var status = PlayerCore.Instance.cursave.missions[i].status;
+                        switch (comparisonMode)
+                        {
+                            case 0: return (status == Mission.MissionStatus.Inactive) ? 0 : 1;
+                            case 1: return (status == Mission.MissionStatus.Ongoing) ? 0 : 1;
+                            case 2: return (status == Mission.MissionStatus.Complete) ? 0 : 1;
+                            default:
+                                return 0;
+                        }
+                    }
+                }
+                return 0;
             }
             else
             {
