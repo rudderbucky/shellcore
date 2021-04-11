9bd599304a29c890b4a1e10b0edd9ddb9c880ed8
rudderbucky <anandkwork7@gmail.com>
2021-04-11 09:55:30 -0500

    Rotation, counter drone fixes

--- Assets/Scripts/Game Object Definitions/Entity Definitions/Craft.cs
+++ Assets/Scripts/Game Object Definitions/Entity Definitions/Craft.cs
@@ -187,7 +187,7 @@ public abstract class Craft : Entity
         bool direction = delta < 90;
 
         //rotate with physics
-        float rotationAmount = Mathf.Min(enginePower / entityBody.mass * Time.deltaTime, 45f);
+        float rotationAmount = Mathf.Min(physicsAccel * Time.deltaTime * 2, 45f);
         entityBody.transform.Rotate(0, 0, (direction ? 2 : -2) * rotationAmount);
 
         //check if the angle has gone over the target
