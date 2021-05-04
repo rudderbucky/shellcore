5177670354fb86dd173725dcbb434c6788899e90
rudderbucky <anandkwork7@gmail.com>
2021-05-04 09:13:53 -0500

    Bugfix multiple retreats not working correctly

--- Assets/Scripts/Functional Definitions/Abilities/Retreat.cs
+++ Assets/Scripts/Functional Definitions/Abilities/Retreat.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Respawns the entity
@@ -22,8 +24,30 @@ public class Retreat : Ability
         if (Core is Craft)
         {
             AudioManager.PlayClipByID("clip_activateability", transform.position);
+            // get all current retreats, set the new retreat's start times to the old retreat start times, find one that is off CD
+            // and set it to CD
+            List<Retreat> oldRetreats = new List<Retreat>();
+            foreach(Ability ability in Core.GetAbilities())
+            {
+                if(ability as Retreat)
+                    oldRetreats.Add(ability as Retreat);
+            }
+
             (Core as Craft).Respawn();
-            Retreat r = Core.GetAbilities().First((a) => { return a is Retreat; }) as Retreat;
+
+            List<Retreat> newRetreats = new List<Retreat>();
+            foreach(Ability ability in Core.GetAbilities())
+            {
+                if(ability as Retreat)
+                    newRetreats.Add(ability as Retreat);
+            }
+            for(int i = 0; i < oldRetreats.Count; i++)
+            {
+               newRetreats[i].startTime = oldRetreats[i].startTime; 
+            }
+
+            // prior to Awake activation since Respawn was set on this stack. We therefore search for 0 here
+            Retreat r = newRetreats.Find((a) => { return a.startTime == 0; }) as Retreat;
             if (r != null)
             {
                 r.startTime = Time.time - activeDuration + 0.1f;
