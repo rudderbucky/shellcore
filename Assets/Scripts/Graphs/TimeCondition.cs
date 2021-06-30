a81e73eabbe6ba4d27de274bd31676bf59786f2c
BaegEhl <bagelyeeter69420@gmail.com>
2021-06-29 21:42:52 -0500

    floats for time

--- Assets/Scripts/Graphs/TimeCondition.cs
+++ Assets/Scripts/Graphs/TimeCondition.cs
@@ -18,13 +18,13 @@ namespace NodeEditorFramework.Standard
         [ConnectionKnob("Output", Direction.Out, "Condition", NodeSide.Right)]
         public ConnectionKnob output;
 
-        public int seconds = 0;
+        public float seconds = 0;
         Coroutine timer = null;
 
         public override void NodeGUI()
         {
             output.DisplayLayout();
-            seconds = Utilities.RTEditorGUI.IntField("Time: ", seconds);
+            seconds = Utilities.RTEditorGUI.FloatField("Time: ", seconds);
         }
 
         public void Init(int index)
