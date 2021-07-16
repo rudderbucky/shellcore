9c3a08c9f45873cf4666e9ef04a651c407ecacc4
FoeFear <79296679+FoeFear@users.noreply.github.com>
2021-07-16 05:38:12 -0700

    Shooter Sprites Update (Fix) (#76)

--- Assets/Scripts/Functional Definitions/Abilities/AbilityUtilities.cs
+++ Assets/Scripts/Functional Definitions/Abilities/AbilityUtilities.cs
@@ -237,15 +237,20 @@ public static class AbilityUtilities
             case 34:
                 return null;
             case 4:
-                return "beamshooter_sprite";
+		if (data != "beamgroundshooter_sprite")
+                {
+                    return "beamshooter_sprite";
+                }
+                else
+                {
+                    return "beamgroundshooter_sprite";
+                }
             case 5:
-            case 14:
             case 15:
                 return "bulletshooter_sprite";
             case 6:
                 return "cannonshooter_sprite";
             case 7:
-            case 36:
             case 37:
                 if (data != "missile_station_shooter")
                 {
@@ -261,6 +266,34 @@ public static class AbilityUtilities
                 return "lasershooter_sprite";
             case 38:
                 return "flakshooter_sprite";
+	    case 36:
+		return "bombshooter_sprite";
+	    case 14:
+		 if (data == "siegegroundshooter_sprite")
+		 {
+		 return "siegegroundshooter_sprite";
+		 }
+		 else if (data != "siegeshooter_sprite")
+		 {
+		 return "bulletshooter_sprite";	 
+		 }
+		 else
+		 {
+		 return "siegeshooter_sprite";
+		 }
+	    case 2:
+		return "ability_indicator_shell";
+	    case 11:
+	    case 12:
+		return "ability_indicator_core";
+	    case 28:
+		return "ability_indicator_retreat";
+	    case 25:
+		return "ability_indicator_damage_boost";
+	    case 27:
+		return "ability_indicator_pin_down";
+	    case 29:
+		return "ability_indicator_absorb_field";
             default:
                 return "ability_indicator";
         }
