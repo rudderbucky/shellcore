d29bd96387d95e5aec2b85b13d0d45be11b8ad3e
FoeFear <mariocartseven@gmail.com>
2021-08-13 18:17:23 -0700

    Revert to Original Tier

--- Assets/Scripts/Functional Definitions/Abilities/AbilityButtonScript.cs
+++ Assets/Scripts/Functional Definitions/Abilities/AbilityButtonScript.cs
@@ -116,7 +116,7 @@ public class AbilityButtonScript : MonoBehaviour, IPointerClickHandler, IPointer
 
     void ReflectTier(Ability ability)
     {
-        if (ability.GetTier() > 0 && ability.GetTier() <= 5)
+        if (ability.GetTier() > 0 && ability.GetTier() <= 3)
         {
             abilityTier.enabled = true;
             abilityTier.sprite = ResourceManager.GetAsset<Sprite>("AbilityTier" + ability.GetTier());
