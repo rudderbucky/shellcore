4da5084be81aca28ba3a906395780685fa13bee3
Ormanus <osalminenster@gmail.com>
2021-08-16 15:57:53 +0300

    DialogueConditionCheck inherits ConditionCheckNode

--- Assets/Scripts/Graphs/ConditionCheckNode.cs
+++ Assets/Scripts/Graphs/ConditionCheckNode.cs
@@ -1,7 +1,6 @@
 ﻿using NodeEditorFramework.Utilities;
 using UnityEngine;
 
-// TODO: Add dialogue mode to this node, remove Dialogue condition node
 namespace NodeEditorFramework.Standard
 {
     [Node(false, "Flow/Condition Check Node", typeof(QuestCanvas), typeof(SectorCanvas))]
@@ -31,39 +30,38 @@ namespace NodeEditorFramework.Standard
         {
             get { return true; }
         }
-
-        [ConnectionKnob("Input", Direction.In, "TaskFlow", NodeSide.Left)]
+        
         public ConnectionKnob input;
-
-        [ConnectionKnob("Pass", Direction.Out, "TaskFlow", ConnectionCount.Single, NodeSide.Right, 20)]
         public ConnectionKnob outputPass;
-
-        [ConnectionKnob("Fail", Direction.Out, "TaskFlow", ConnectionCount.Single, NodeSide.Right, 60)]
         public ConnectionKnob outputFail;
+
+        ConnectionKnobAttribute inputStyle = new ConnectionKnobAttribute("Input", Direction.In, "TaskFlow", NodeSide.Left);
+        ConnectionKnobAttribute outputPassStyle = new ConnectionKnobAttribute("Pass", Direction.Out, "TaskFlow", ConnectionCount.Single, NodeSide.Right, 20);
+        ConnectionKnobAttribute outputFailStyle = new ConnectionKnobAttribute("Fail", Direction.Out, "TaskFlow", ConnectionCount.Single, NodeSide.Right, 60);
 
         public string variableName = "";
         public int variableType = 0;
         public int comparisonMode = 0;
         public int value = 0;
 
-        PopupMenu typePopup = null;
-        PopupMenu comparisonPopup = null;
+        protected PopupMenu typePopup = null;
+        protected PopupMenu comparisonPopup = null;
 
-        readonly string[] comparisonModes = new string[]
+        protected readonly string[] comparisonModes = new string[]
         {
             "EqualTo",
             "GreaterThan",
             "LesserThan"
         };
 
-        readonly string[] missionStatus = new string[]
+        protected readonly string[] missionStatus = new string[]
         {
             "Inactive",
             "Ongoing",
             "Complete"
         };
 
-        readonly string[] variableTypes = new string[]
+        protected readonly string[] variableTypes = new string[]
         {
             "Checkpoint",
             "Task Variable",
@@ -74,8 +72,23 @@ namespace NodeEditorFramework.Standard
             "Shards"
         };
 
+        public virtual void InitConnectionKnobs()
+        {
+            if (input == null)
+            {
+                input = CreateConnectionKnob(inputStyle);
+                outputPass = CreateConnectionKnob(outputPassStyle);
+                outputFail = CreateConnectionKnob(outputFailStyle);
+            }
+        }
+
         public override void NodeGUI()
         {
+            if (input == null)
+            {
+                InitConnectionKnobs();
+            }
+
             GUILayout.BeginHorizontal();
             GUILayout.Label("Pass: ");
             outputPass.DrawKnob();
