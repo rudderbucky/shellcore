d10d33ea923881852229573acd436baa4e9cf0c8
ctrlraul <mailctrlraul@gmail.com>
2022-07-15 00:10:12 -0300

    Fix bug in ShipEditor zoom with proper "mouse over" check.

---
+++ Assets/Scripts/PointerOverDetector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointerOverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool isPointerOver = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerOver = false;
+    }
+
+}
