553eec2794e0a1f2099a14fa98bc03a027843076
rudderbucky <anandkwork7@gmail.com>
2023-04-15 14:52:09 -0700

    Update DevConsoleScript.cs

--- Assets/Scripts/HUD Scripts/DevConsoleScript.cs
+++ Assets/Scripts/HUD Scripts/DevConsoleScript.cs
@@ -33,6 +33,7 @@ public class DevConsoleScript : MonoBehaviour
         Application.logMessageReceived += HandleLog;
         godModeEnabled = false;
         spectateEnabled = false;
+        partsObtainedCheat = false;
         Instance = this;
         componentEnabled = false;
     }
