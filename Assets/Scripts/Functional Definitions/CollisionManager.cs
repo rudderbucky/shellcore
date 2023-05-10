fb09cd099d3884c77e0eb6e747f04c001d4ed7c4
rudderbucky <anandkwork7@gmail.com>
2023-05-09 21:25:40 -0700

    Include player in collisions

--- Assets/Scripts/Functional Definitions/CollisionManager.cs
+++ Assets/Scripts/Functional Definitions/CollisionManager.cs
@@ -241,9 +241,7 @@ public class CollisionManager : MonoBehaviour
                     continue;
                 if (entity.GetInvisible())
                     continue;
-                if (entity == PlayerCore.Instance)
-                    continue;
-
+                    
                 Vector2[] colliders = _colliders[i];
                 for (int j = 0; j < colliders.Length / 4; j++)
                 {
