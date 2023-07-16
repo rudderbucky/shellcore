48b7187070404be2f4a42cdf1c5c34c3aed398de
rudderbucky <anandkwork7@gmail.com>
2023-07-15 17:43:49 -0700

    Relax constraint on missions with prerequisites

--- Assets/Scripts/Graphs/TaskManager.cs
+++ Assets/Scripts/Graphs/TaskManager.cs
@@ -333,7 +333,7 @@ public class TaskManager : MonoBehaviour, IDialogueOverrideHandler
     {
         if (!offloadingMissions.ContainsKey(missionName)) return;
         var mission = SaveHandler.instance.GetSave().missions.Find(m => m.name == missionName);
-        if (mission.prerequisites != null && SaveHandler.instance.GetSave().missions.Exists(m =>
+        if (mission.prerequisites != null && !SaveHandler.instance.GetSave().missions.TrueForAll(m =>
             mission.prerequisites.Contains(m.name)
                 && m.status != Mission.MissionStatus.Complete))
             return;
@@ -414,6 +414,9 @@ public class TaskManager : MonoBehaviour, IDialogueOverrideHandler
                     }
                 }
 
+                if (traverser.findRoot().prerequisites != null)
+                    mission.prerequisites = new List<string>(traverser.findRoot().prerequisites.ToArray());
+                    
                 if (traverser.findRoot().overrideCheckpoint)
                 {
                     traverser.activateCheckpoint(traverser.findRoot().overrideCheckpointName);
