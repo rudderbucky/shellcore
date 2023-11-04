a0b3b3ba3bdd6ead65e3cc6ddde1311673ac30ba
rudderbucky <anandkwork7@gmail.com>
2023-11-04 12:43:19 -0700

    Fix infinite callback glitch on pancamera

--- Assets/Scripts/SFX Scripts/CameraScript.cs
+++ Assets/Scripts/SFX Scripts/CameraScript.cs
@@ -123,6 +123,7 @@ public class CameraScript : MonoBehaviour
             if (callback != null)
             {
                 callback.Invoke();
+                callback = null;
             }
         }
     }
