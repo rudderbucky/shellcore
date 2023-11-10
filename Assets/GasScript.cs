952b7c07fc8932030364d3f69e8b2dc93227082e
rudderbucky <anandkwork7@gmail.com>
2023-11-10 15:52:16 -0800

    Init commit

---
+++ Assets/GasScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasScript : MonoBehaviour
+{
+    public float orbitalZ;
+    public float angularVelocity;
+    public float radial;
+    public int emissionPerSecond = 10;
+    public float radius;
+    public float multiplier;
+    public float arc = 0.3333f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arc =  Random.Range(2, 9);
+        angularVelocity = Random.Range(-4f, -64f);
+        radius = Random.Range(5, 10f);
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().angularDrag = 0;
+        GetComponent<Rigidbody2D>().angularVelocity = angularVelocity * multiplier;
+        var partSys = GetComponent<ParticleSystem>();
+        var velo = partSys.velocityOverLifetime;
+        velo.orbitalZ = orbitalZ * Mathf.Sqrt(multiplier);
+        velo.radial = radial * multiplier;
+        var emission = partSys.emission;
+        var shape = partSys.shape;
+        shape.radius = radius * multiplier;
+        shape.arcSpread = 1 / arc;
+        emission.rateOverTime = 0 * emissionPerSecond * Mathf.Sqrt(multiplier);
+    }
+}
